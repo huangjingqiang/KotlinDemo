4cb25544a81eb2d64aac7a96fb3aa68428498f4a
hjq <hjq@Gmail.com>
2017-04-18 18:27:42 +0800

    add tab click

--- app/src/main/java/com/hjq/kotlin/MainActivity.java
+++ app/src/main/java/com/hjq/kotlin/MainActivity.java
@@ -1,22 +1,15 @@
 package com.hjq.kotlin;
 
 import android.os.Bundle;
-import android.util.Log;
 
-import com.hjq.kotlin.bean.GankBean;
 import com.hjq.kotlin.fragment.FirstTabFragment;
 import com.hjq.kotlin.fragment.SecondTabFragment;
 import com.hjq.kotlin.fragment.ThreeTabFragment;
-import com.hjq.kotlin.service.ApiService;
-import com.hjq.kotlin.service.ServiceManager;
 import com.hjq.kotlin.view.BottomBar;
 import com.hjq.kotlin.view.BottomBarTab;
 
 import me.yokeyword.fragmentation.SupportActivity;
 import me.yokeyword.fragmentation.SupportFragment;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends SupportActivity {
     private static final int FIRST = 0;
@@ -31,44 +24,46 @@ public class MainActivity extends SupportActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
+        if (savedInstanceState == null){
             FirstTabFragment first = new FirstTabFragment();
             ThreeTabFragment three = new ThreeTabFragment();
             fragments[FIRST] = first.newInstance();
             fragments[SECOND] = SecondTabFragment.newInstance();
             fragments[THIRD] = three.newInstance();
 
-            loadMultipleRootFragment(R.id.fl_tab_container, FIRST,
+            loadMultipleRootFragment(R.id.fl_tab_container,FIRST,
                     fragments[FIRST],
                     fragments[SECOND],
                     fragments[THIRD]);
-        } else {
+        }else {
             fragments[FIRST] = findFragment(FirstTabFragment.class);
             fragments[SECOND] = findFragment(SecondTabFragment.class);
             fragments[THIRD] = findFragment(ThreeTabFragment.class);
         }
         initView();
-
-        ServiceManager.Companion.getApi().getFuLi(1)
-                .subscribeOn(Schedulers.io())
-                .subscribeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<ApiService.ResponseWrapper<GankBean.ResultsBean>>() {
-                    @Override
-                    public void call(ApiService.ResponseWrapper<GankBean.ResultsBean> resultsBeanResponseWrapper) {
-                        Log.e("------1>", resultsBeanResponseWrapper.getResults().toString());
-                    }
-                }, new Action1<Throwable>() {
-                    @Override
-                    public void call(Throwable throwable) {
-                        Log.e("---------2>", throwable.getMessage());
-                    }
-                });
     }
 
     private void initView() {
         bottomBar = (BottomBar) this.findViewById(R.id.bottomBar);
-        bottomBar.addItem(new BottomBarTab(this, R.drawable.ic_android_red_800_24dp, "shouye"))
-                .addItem(new BottomBarTab(this, R.drawable.ic_favorite_border_white_24dp, "shoucang"))
-                .addItem(new BottomBarTab(this, R.drawable.ic_account_circle_white_24dp, "wode"));
+        bottomBar.addItem(new BottomBarTab(this,R.drawable.ic_android_red_800_24dp,"shouye"))
+                .addItem(new BottomBarTab(this,R.drawable.ic_favorite_border_white_24dp,"shoucang"))
+                .addItem(new BottomBarTab(this, R.drawable.ic_account_circle_white_24dp,"wode"));
+        bottomBar.setOnTabSelectedListener(new BottomBar.OnTabSelectedListener(){
+
+            @Override
+            public void onTabSelected(int position, int prePosition) {
+                showHideFragment(fragments[position],fragments[prePosition]);
+            }
+
+            @Override
+            public void onTabUnselected(int position) {
+
+            }
+
+            @Override
+            public void onTabReselected(int position) {
+
+            }
+        });
     }
 }
